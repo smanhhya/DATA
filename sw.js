d8678cfa9f0a52969d3a7f492749b85734803d82
smanhhya <smanhhya@gmail.com>
2026-05-22 21:39:05 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] تم التثبيت');
-});
-
-self.addEventListener('fetch', (e) => {
-    // لن نقوم بتخزين معقد الآن، فقط ليعمل كـ PWA
-});
